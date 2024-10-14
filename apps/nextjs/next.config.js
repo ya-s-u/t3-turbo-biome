208dcb7e6a6f8e0452569673d9332dc9badeec8a
Yasuaki Goto <yasu1003@gmail.com>
2024-10-14 22:07:04 +0900

    fix format

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -6,20 +6,20 @@ await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
-	reactStrictMode: true,
+  reactStrictMode: true,
 
-	/** Enables hot reloading for local packages without a build step */
-	transpilePackages: [
-		"@acme/api",
-		"@acme/auth",
-		"@acme/db",
-		"@acme/ui",
-		"@acme/validators",
-	],
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@acme/api",
+    "@acme/auth",
+    "@acme/db",
+    "@acme/ui",
+    "@acme/validators",
+  ],
 
-	/** We already do linting and typechecking as separate tasks in CI */
-	eslint: { ignoreDuringBuilds: true },
-	typescript: { ignoreBuildErrors: true },
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default config;
--- apps/nextjs/src/trpc/server.ts
+++ apps/nextjs/src/trpc/server.ts
@@ -13,19 +13,19 @@ import { createQueryClient } from "./query-client";
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(async () => {
-	const heads = new Headers(headers());
-	heads.set("x-trpc-source", "rsc");
+  const heads = new Headers(headers());
+  heads.set("x-trpc-source", "rsc");
 
-	return createTRPCContext({
-		session: await auth(),
-		headers: heads,
-	});
+  return createTRPCContext({
+    session: await auth(),
+    headers: heads,
+  });
 });
 
 const getQueryClient = cache(createQueryClient);
 const caller = createCaller(createContext);
 
 export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
-	caller,
-	getQueryClient,
+  caller,
+  getQueryClient,
 );
